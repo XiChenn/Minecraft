128e47e301a8c7287bbd2456c8cfa87c0e7cc605
Xi Chen <xichen.u@gmail.com>
2020-10-20 23:06:58 -0400

    organize files

---
+++ config.py
@@ -0,0 +1,25 @@
+TICKS_PER_SEC = 60
+
+# Size of sectors used to ease block loading.
+SECTOR_SIZE = 16
+
+WALKING_SPEED = 5
+FLYING_SPEED = 15
+
+GRAVITY = 9.8
+MAX_JUMP_HEIGHT = 1.0  # About the height of a block.
+
+TERMINAL_VELOCITY = 50
+
+PLAYER_HEIGHT = 2
+
+TEXTURE_PATH = 'texture.png'
+
+FACES = [
+    (0, 1, 0),
+    (0, -1, 0),
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+]
